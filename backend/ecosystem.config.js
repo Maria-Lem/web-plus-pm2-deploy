5a40e77690d5b1f37cb67531d552e7d26902e741
Maria Alemaskina <maria131621@yandex.ru>
2024-04-04 22:56:23 +0300

    feat: add backend deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+
 dotenv.config({ path: './.env.deploy' });
 
 const {
@@ -10,10 +11,10 @@ const {
 } = process.env;
 
 module.exports = {
-  apps : [
+  apps: [
     {
-      name   : "mesto",
-      script : "dist/app.js"
+      name: 'mesto',
+      script: 'dist/app.js',
     },
   ],
   deploy: {
@@ -23,8 +24,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
       'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
-}
+};
