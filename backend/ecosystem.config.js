cc3657540b7e180cc62f771d9ce98069ec789154
Maria Alemaskina <maria131621@yandex.ru>
2024-04-05 14:55:38 +0300

    feat

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,11 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env.deploy' });
 
 const {
-  DEPLOY_USER,
-  DEPLOY_HOST,
-  DEPLOY_PATH,
-  DEPLOY_REF,
-  DEPLOY_REPOSITORY,
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY,
 } = process.env;
 
 module.exports = {
@@ -19,13 +15,15 @@ module.exports = {
   ],
   deploy: {
     production: {
+      // key: '~/.ssh/id_rsa.pub',
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy':
+        'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
